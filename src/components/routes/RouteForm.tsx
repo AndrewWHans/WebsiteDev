da70a8aef565554139b6c3ef86bbc3b3add9655c
ULimo <ulimohelp@gmail.com>
2025-05-24 14:12:55 -0400

    Deploy application to Netlify

--- src/components/routes/RouteForm.tsx
+++ src/components/routes/RouteForm.tsx
@@ -185,6 +185,7 @@ export const RouteForm = ({
                     dateFormat="h:mm aa"
                     autoComplete="off"
                     shouldCloseOnSelect={true}
+                    onChangeRaw={(e) => e.preventDefault()}
                     className="block w-full px-3 py-2 sm:text-sm border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
                     placeholderText="Add time slot"
                   />
