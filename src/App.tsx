8d2b4e59b105dbaff7b16cad2b4bd2ecb0960de1
ULimo <ulimohelp@gmail.com>
2025-07-02 03:31:46 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ import { AdminDashboard } from './components/AdminDashboard';
 import { PrivateRidePage } from './components/PrivateRidePage';
 import { VerifyTicketPage } from './components/VerifyTicketPage';
 import { AboutPage } from './components/AboutPage';
-import { DriverPortal } from './components/DriverPortal';
+import { DriverPortal } from './components/driver/DriverPortal';
 import { DealsPage } from './components/DealsPage';
 import { FeedbackPage } from './components/FeedbackPage';
 import { PageTransition } from './components/shared/PageTransition';
